4b6c512b5e125029dc31734d10cc1c7043e82d36
Collins Lekakeny <lekakenycollins8@gmail.com>
2025-01-24 12:42:14 +0300

    Add CORS middleware to handle allowed origins

---
+++ server/middleware/cors.js
@@ -0,0 +1,17 @@
+const cors = require('cors');
+
+// Whitelist allowed origins
+const whitelist = ['http://localhost:3000'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+
+module.exports = cors(corsOptions);
