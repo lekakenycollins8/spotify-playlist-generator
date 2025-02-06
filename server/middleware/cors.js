1a0c944c628a74c90002cb38b8ce2c96f75da832
Collins Lekakeny (Leky) <138676075+lekakenycollins8@users.noreply.github.com>
2025-02-06 16:46:31 +0300

    whitelisted server URL in cors

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,8 +1,10 @@
 const cors = require('cors');
 
 // Whitelist allowed origins
-const whitelist = ['https://spotify-playlist-generator-client-nine.vercel.app/'];
-
+const whitelist = [
+  "https://spotify-playlist-generator-client-nine.vercel.app", // Client URL
+  "https://spotify-playlist-generator-server.vercel.app" // Server URL
+];
 const corsOptions = {
     origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
